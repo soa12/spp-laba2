10fda03064469eb6fb3629712aaa0f7421d47e4f
Sabyrzhan Aspombitov <sabyrzhan.aspombitov@corp.oft-e.com>
2018-10-31 23:43:13 +0700

    set up app

--- src/app.php
+++ src/app.php
@@ -5,3 +5,37 @@
  * Date: 31.10.18
  * Time: 22:55
  */
+
+//use Signkeeper\Controllers;
+
+require_once __DIR__ . '../../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => TRUE,
+    ]
+]);
+
+$container = $app->getContainer();
+
+$container['logger'] = function ($container) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler(__DIR__ . "/../../logs/laba2/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+/*$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('templates', [
+        'cache' => 'path/to/cache'
+    ]);
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+    return $view;
+};*/
+
+$container['view'] = function ($container) {
+    return new \Slim\Views\PhpRenderer('templates');
+};
+
+require_once __DIR__ . '/routes.php';
--- src/routes.php
+++ src/routes.php
@@ -5,3 +5,29 @@
  * Date: 31.10.18
  * Time: 22:56
  */
+
+$app->get('/', function ($request, $response) {
+    $this->logger->addInfo('Начальная страница');
+    $templateVariables = array('startLabel' => 'Приложение запущено');
+    $response = $this->view->render($response, 'app.phtml', ['variables' => $templateVariables]);
+    return $response;
+});
+
+$app->get('/action', function ($request, $response) {
+    $params = $request->getQueryParams();
+    $this->logger->addInfo('Нажата кнопка ' . $params['submit']);
+    $templateVariables = array('startLabel' => $params['submit']);
+    $response = $this->view->render($response, 'app.phtml', ['variables' => $templateVariables]);
+    return $response;
+});
+
+$app->get('/logs', function ($request, $response) {
+    $this->logger->addInfo('Просмотр логов');
+    $logs = file(__DIR__ .'/../../logs/app.log');
+    $response = $this->view->render($response, 'logs.phtml', ['variables' => $logs]);
+    return $response;
+});
+
+$app->get('/start', function ($request, $response) {
+    echo 'hello world';
+});
