b4ae29c15bee2bc5404947076a46c4eaa4dda127
Sabyrzhan Aspombitov <sabyrzhan.aspombitov@corp.oft-e.com>
2018-10-31 22:58:10 +0700

    created slim app

---
+++ src/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sabyrzhan
+ * Date: 31.10.18
+ * Time: 22:55
+ */
---
+++ src/public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sabyrzhan
+ * Date: 25.10.18
+ * Time: 23:42
+ */
+
+require_once __DIR__ . '/../app.php';
+
+$app->run();
